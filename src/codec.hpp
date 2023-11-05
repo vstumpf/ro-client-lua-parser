91506b5b3d97bd5329addd7b6ed4c77317b0a50f
Vincent Stumpf <vincents.995@gmail.com>
2023-11-05 00:59:28 -0700

    Add decode/encode to parse korean

---
+++ src/codec.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+std::string decode1252(std::string str);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
+#include <fstream>
 #include <iostream>
 
+#include <nlohmann/json.hpp>
 #include <sol/sol.hpp>
 
 #include "args.hpp"
+#include "codec.hpp"
+
+using json = nlohmann::json;
 
 int parseItemInfo(std::string filename) {
 	std::cout << "Parsing item info from " << filename << "\n";
@@ -11,21 +16,60 @@ int parseItemInfo(std::string filename) {
 	lua.open_libraries(sol::lib::base, sol::lib::package);
 
 	lua.script_file(filename);
+
+	json j;
+	j["iteminfo"] = json::object();
+
 	// pcall(AddItem, ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName,
 	// DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
-	lua["AddItem"] = [](uint32_t itemid, std::string unid_name, std::string unid_resc,
-						std::string id_name, std::string id_resc, uint32_t slot_count,
-						uint32_t class_num) {
-		std::cout << "AddItem(" << itemid << ", " << unid_name << ", " << unid_resc << ", "
-				  << id_name << ", " << id_resc << ", " << slot_count << ", " << class_num << ")\n";
+	lua["AddItem"] = [&j](uint32_t itemid, std::string unid_name, std::string unid_resc,
+						  std::string id_name, std::string id_resc, uint32_t slot_count,
+						  uint32_t class_num) {
+		// std::cout << "AddItem(" << itemid << ", " << unid_name << ", " << unid_resc << ", "
+		// 		  << id_name << ", " << id_resc << ", " << slot_count << ", " << class_num <<
+		// ")\n";
+
+		json::object_t item;
+
+		j["iteminfo"][std::to_string(itemid)] = {{"id", itemid},
+												 {"unid_name", decode1252(unid_name)},
+												 {"unid_resc", decode1252(unid_resc)},
+												 {"id_name", decode1252(id_name)},
+												 {"id_resc", decode1252(id_resc)},
+												 {"slot_count", slot_count},
+												 {"class_num", class_num},
+												 {"unid_desc", json::array()},
+												 {"id_desc", json::array()}};
+
 		return std::tuple<bool, std::string>(true, "");
 	};
 
-	lua["AddItemUnidentifiedDesc"] = [](uint32_t itemid, std::string description) {
+	lua["AddItemUnidentifiedDesc"] = [&j](uint32_t itemid, std::string description) {
+		try {
+			j["iteminfo"][std::to_string(itemid)]["unid_desc"].push_back(decode1252(description));
+		} catch (std::exception& e) {
+			std::cout << itemid << "" << description << "\n";
+			std::cout << e.what() << "\n";
+		}
 		return std::tuple<bool, std::string>(true, "");
 	};
 
-	lua["AddItemIdentifiedDesc"] = [](uint32_t itemid, std::string description) {
+	lua["AddItemIdentifiedDesc"] = [&j](uint32_t itemid, std::string description) {
+		try {
+			j["iteminfo"][std::to_string(itemid)]["id_desc"].push_back(decode1252(description));
+		} catch (std::exception& e) {
+			std::cout << description << "\n";
+			std::cout << e.what() << "\n";
+		}
+
+		return std::tuple<bool, std::string>(true, "");
+	};
+
+	lua["AddItemIsCostume"] = [](uint32_t itemid, bool is_costume) {
+		return std::tuple<bool, std::string>(true, "");
+	};
+
+	lua["AddItemEffectInfo"] = [](uint32_t itemid, uint32_t effect_id) {
 		return std::tuple<bool, std::string>(true, "");
 	};
 
@@ -35,6 +79,8 @@ int parseItemInfo(std::string filename) {
 		sol::error err = result;
 		std::cout << err.what();
 	}
+
+	std::ofstream("iteminfo.json") << j.dump(2);
 
 	return 0;
 }
